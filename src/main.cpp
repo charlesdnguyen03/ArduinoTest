9b69f4201a91a4a10f5dc063321cd59141210054
Charles Nguyen <charlesnguyentheguy@gmail.com>
2023-02-08 15:12:16 -0800

    Basic PID

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,46 @@
-// ------BLDC--------
-#include <Servo.h>
-#define MAX_PWM 2000
-#define MIN_PWM 1000
-#define STATIONARY_PWM 1500
-Servo ESC;
-double motorSpeed = 0;
-// ------------------
-
-// ------BNO055-----
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-#define MPU_ADDRESS 0x28 // I2C address of the BNO055
-#define BNO055_SAMPLERATE_DELAY_MS 10
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, MPU_ADDRESS, &Wire);
-double yawAngle = 0;
-double yawAngularSpeed = 0;
-// ------------------
+#include <Servo.h>
 
-// -------PID--------
-#include "PID.h"
-const double xkP = 2.5; // displacement kP 
-const double xkI = 0.0;
-const double xkD = 400; // 400 kD lmao
-PIDAngleController pidAngle(xkP, xkI, xkD); 
-const double vkP = 0.050; // velocity kP // TODO: ESC already has a velocity speed, so sus.
-const double vkI = 0.000;
-const double vkD = 0.017; 
-PIDController pidSpeed(vkP, vkI, vkD);
-// ------------------
+// MARCIN'S VERY BARE-BONES TEST-STAND PID
 
-// ------GENERAL-----
-long timeCur, timePrev, timeStart; 
-const int numReadings = 5;
-double readings[numReadings];
-int readIndex = 0;
-double total = 0;
-double rollingAvg = 0;
-double targetPos = 0;
-// FSM variables
-byte controllerState = 0;
-// ------------------
 
-// Calibrate ESC by plugging in battery when MAX_PWM is being outputted.
+//   BNO055 Connections
+//   ===========
+//   CSCL --- A5
+//   SDA ---- A4
+//   VDD ---- 3.3-5V DC
+//   GND ---- Common Ground
+
+
+// ESC Connections
+// ================
+// Dark ----- GND
+// Yellow --- 9
+
+
+// Delay between loops
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+
+// PWM where the motor doesn't spin
+#define STATIONARY_PWM (1500)
+
+// Maximum PWM
+#define MAX_PWM (2000)
+
+// Minimum PWM
+#define MIN_PWM (1000)
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+float target_pos; // Position for PID to aim at
+
+Servo ESC; // Pretending our ESC is a servo
+
+// Calibrate ESC by plugging in battery when
+// MAX_PWM is being outputted.
 void calibrateESC() {
   ESC.writeMicroseconds(MAX_PWM);
   delay(8000);
@@ -51,102 +48,56 @@ void calibrateESC() {
   delay(1000);
 }
 
-// Set the current speed and direction of the motor
-void setSpeed(double targetSpeed) {
-  // the question is what the difference between stationary and min is.
-  motorSpeed = constrain(STATIONARY_PWM + targetSpeed, MIN_PWM, MAX_PWM); 
-  ESC.writeMicroseconds(motorSpeed);
-}
-
-void updateYawAngle() {
-  // imu::Vector<3> eulerAngles = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  // return eulerAngles.z();
-  sensors_event_t event;
-  bno.getEvent(&event);
-  yawAngle = event.orientation.roll;
-}
-
-void updateYawSpeed() {
-  // imu::Vector<3> angularSpeed = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-  // return angularSpeed.z() * 57.2958; // For degrees per second (originally in radians per second)
-  sensors_event_t event;
-  bno.getEvent(&event);
-  yawAngularSpeed = event.gyro.x;
-}
-
-// Smooth the angular speed --> rolling average
-void updateRollingAvg() {
-    total = total - readings[readIndex]; 
-    readings[readIndex] = yawAngularSpeed; 
-    total = total + readings[readIndex];
-    readIndex = readIndex + 1;
-    if (readIndex >= numReadings) { readIndex = 0; }
-    rollingAvg = total / numReadings;
+// Displays orientation
+void displaySensorDetails(sensors_event_t &event) {
+  Serial.print("Orientation.roll "); Serial.println(event.orientation.roll);
+  // Serial.print("Orientation.azimuth "); Serial.println(event.orientation.azimuth);
+  Serial.print("Orientation.pitch "); Serial.println(event.orientation.pitch);
 }
 
 void setup() {
   Serial.begin(9600);
   
-  // IMU setup
+  // Set up the IMU
   if(!bno.begin()) {
-    Serial.print(F("No BNO055 detected"));
+    Serial.println("BNO055 not detected! Quitting!");
     exit(1);
   }
   bno.setExtCrystalUse(true);
-  // this isn't in Marcin's code so I assume it's okay to leave out
-  // // Calibrating IMU
-  // Serial.print(F("Calibrating IMU"));
-  // while (true) {
-  //   uint8_t system, gyro, accel, mag = 0;
-  //   if (system) { break; } // Calibrated when system > 0 (Fully when system == 3)
 
-  //   Serial.print(F("."));
-  //   delay(50);
-  // }
-  
-  // Start ESC on pin 9
-  ESC.attach(9, MIN_PWM, MAX_PWM);
+  // Set up the ESC
+  ESC.attach(9,1000,2000); // (pin, min pulse width, max pulse width in microseconds) 
   ESC.writeMicroseconds(STATIONARY_PWM);
-  
-  updateYawAngle();
-  targetPos = yawAngle;
+
+  // Set the target_pos to current position
+  sensors_event_t event;
+  bno.getEvent(&event);
+  target_pos = event.orientation.roll;
 
   Serial.println("Setup finished!");
-
-  timeCur = millis();
-  timeStart = timeCur;
 }
 
 void loop() {
-  // Every 10ms, read IMU and call controllers
-  if (millis() - timeCur > 10) {
-    timePrev = timeCur;
-    timeCur = millis();
+  // store sensor data in "event"
+  sensors_event_t event;
+  bno.getEvent(&event);
 
-    // update values
-    updateYawAngle();
-    updateYawSpeed();
-    updateRollingAvg();
+  float vel = event.gyro.x; // gyro gives velocity
+  float pos = event.orientation.roll;
 
-    // FSM transition
-    if (controllerState == 1 && fabs(rollingAvg) > 360 /* °/s */) {
-      controllerState = 0;
-    } else if (controllerState == 0 && fabs(rollingAvg) < 45 /* °/s */) {
-      controllerState = 1;
-    }
-    // FSM action 
-    if (controllerState == 0) { // state 0 = detumble + update time for angle 
-      pidAngle.compute(targetPos, yawAngle, timeCur - timePrev); 
-    } else { // state 1 = set setpoint to calculated motor output. Error = desired pwm -avg pwm 
-      motorSpeed += pidSpeed.compute(pidAngle.compute(targetPos, yawAngle, timeCur - timePrev), rollingAvg, timeCur - timePrev);
-    } 
-    setSpeed(motorSpeed);
+  float P = 0.5 * (pos - target_pos);
+  float I = 0; // set to zero for now
+  float D = -0.0 * (vel); // set to zero for now
 
-    delay(BNO055_SAMPLERATE_DELAY_MS);
+  float pwm_out = STATIONARY_PWM + P + I + D;
 
-    // Print info to console
-    Serial.print(yawAngle);
-    Serial.print(F(" "));
-    Serial.println(rollingAvg);
-  }
+  // constrain pwm to range
+  pwm_out = constrain(pwm_out, MIN_PWM, MAX_PWM);
+
+  
+  Serial.print("Outputting PWM: "); Serial.println(pwm_out);
+
+  ESC.writeMicroseconds(pwm_out);
+
+  delay(BNO055_SAMPLERATE_DELAY_MS);
 }
